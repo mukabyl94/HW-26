ed451c8d759cccf07eba03771bb78b2e30463a78
mukabyl <mukabyl@yamdex.ru>
2023-02-24 17:12:59 +0300

    first commit

--- src/Box.java
+++ src/Box.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Box {
+public class Box <A>{
+    private A age;
+
+    public Box(A age) {
+        this.age = age;
+    }
+
+    public Box() {
+    }
+
+    public A getAge() {
+        return age;
+    }
+
+    public void setAge(A age) {
+        this.age = age;
+    }
+    public static <A> A method(){
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "age=" + age +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Box<Integer> boxs = new Box<>(25);
+        System.out.println(boxs);
+        Box.method();
     }
 }
